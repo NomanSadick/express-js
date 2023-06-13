79f28e70dcb6716ea46e224a5deca0139be46652
Noman Sadick <nomansadickjs@gmail.com>
2023-06-13 19:30:29 +0600

    status manupulate

--- server.js
+++ server.js
@@ -2,21 +2,12 @@ const express = require ( 'express' );
 
 app = express();
 
-app.get('/', function (req, res) {
-    res.send('Welcome to the express')
+app.get('/one', function (req, res) {
+    res.status(401);
+    res.end("Unauthorized");
 })
 
-app.post('/home', function (req, res) {
-    res.send('Welcome to the express home page')
-})
 
-app.put('/about', function (req, res) {
-    res.send('Welcome to the express about page')
-})
-
-app.delete('/contract', function (req, res) {
-    res.send('Welcome to the express contract page')
-})
 
 
 app.listen(8000, function () {
